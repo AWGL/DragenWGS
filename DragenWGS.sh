ccba42c062b66b6fa7c8224d6f5ad79aec750ce1
SophieS9 <sophie.shaw@wales.nhs.uk>
2021-08-16 14:01:59 +0100

    Updating reference genome assignment to be from variables

--- DragenWGS.sh
+++ DragenWGS.sh
@@ -16,7 +16,6 @@ version=2.0.0
 ##############################################
 
 pipeline_dir="/data/diagnostics/pipelines/"
-dragen_ref="/staging/resources/human/reference/GRCh37"
 output_dir="/Output/results/"
 
 
@@ -77,7 +76,7 @@ fi
 
 # run sample level script
 
-bash commands/run_dragen_per_sample.sh $seqId $sampleId $pipelineName $pipelineVersion $panel $dragen_ref
+bash commands/run_dragen_per_sample.sh $seqId $sampleId $pipelineName $pipelineVersion $panel $dragen_ref $assembly
 
 touch "$seqId"_"$sampleId".mapping_metrics.csv
 
