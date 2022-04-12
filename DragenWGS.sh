5922aeb97d28261e8d6e2a465a352830252e291a
Christopher Medway <josephhalstead89@gmail.com>
2022-04-12 18:14:34 +0100

    bump version

--- DragenWGS.sh
+++ DragenWGS.sh
@@ -9,7 +9,7 @@ ulimit -S -n 65535
 
 # Usage: cd /staging/data/results/$seqId/$panel/$sampleId && bash DragenWGS.sh 
 
-version=2.1.0
+version=2.2.0
 
 ##############################################
 # SETUP                                      #
