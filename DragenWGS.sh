246bc8c8267c54ce15927d586433f09c85f6d750
SophieS9 <sophie.shaw@wales.nhs.uk>
2022-07-01 15:41:58 +0100

    bump version

--- DragenWGS.sh
+++ DragenWGS.sh
@@ -9,7 +9,7 @@ ulimit -S -n 65535
 
 # Usage: cd /staging/data/results/$seqId/$panel/$sampleId && bash DragenWGS.sh 
 
-version=2.3.0
+version=2.4.0
 
 ##############################################
 # SETUP                                      #
